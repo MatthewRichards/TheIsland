637d5d43f76e69ae5ec3ded7610ab2853171f4a0
Matthew Richards <matthew.richards@softwire.com>
2017-06-03 21:37:09 +0100

    Add missing files

---
+++ Island/Activities/Do.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Island.Actors;
+using Island.Models;
+using Island.Resources;
+
+namespace Island.Activities
+{
+  public static class Do
+  {
+    public static Activity Collect<TResource>() where TResource : Resource
+    {
+      return new Doer((actor, state) =>
+      {
+        int capacity = ((Person)actor).CanCarry<TResource>();
+        int amountCollected = state.Collect<TResource>(capacity);
+        ((Person)actor).PickUp<TResource>(amountCollected);
+      });
+    }
+
+    public static Activity Harvest<TResource>() where TResource : Resource
+    {
+      return new Doer((actor, state) =>
+      {
+        state.Harvest<TResource>();
+      });
+    }
+
+    public static Activity DropOff<TResource>(int amount) where TResource : Resource
+    {
+      return new Doer((actor, state) =>
+      {
+        int amountDropped = ((Person)actor).Drop<TResource>(amount);
+        state.DropOff<TResource>(amountDropped);
+      });
+    }
+
+    private class Doer : Activity
+    {
+      private readonly Action<Actor, WorldView> action;
+
+      public Doer(Action<Actor, WorldView> action)
+      {
+        this.action = action;
+      }
+
+      public override void Act(Actor actor, WorldView state)
+      {
+        action(actor, state);
+      }
+    }
+  }
+}
---
+++ Island/Models/WorldView.cs
@@ -0,0 +1,54 @@
+﻿using Island.Actors;
+using Island.Resources;
+
+namespace Island.Models
+{
+  public class WorldView
+  {
+    private readonly World world;
+    private Location location;
+
+    public WorldView(World world, Location location)
+    {
+      this.world = world;
+      this.location = location;
+    }
+
+    public Location Location => location;
+
+    public int Harvest<TResource>() where TResource : Resource
+    {
+      return world.Harvest<TResource>(location);
+    }
+
+    public int CanHarvest<TResource>() where TResource : Resource
+    {
+      return world.CanHarvest<TResource>(location);
+    }
+
+    public int Collect<TResource>(int amount) where TResource : Resource
+    {
+      return world.Collect<TResource>(location, amount);
+    }
+
+    public int CanCollect<TResource>() where TResource : Resource
+    {
+      return world.CanCollect<TResource>(location);
+    }
+
+    public void DropOff<TResource>(int amount) where TResource : Resource
+    {
+      world.DropOff<TResource>(location, amount);
+    }
+
+    public bool IsAccessibleTo(Location newLocation, Actor actor)
+    {
+      return world.IsAccessibleTo(newLocation, actor);
+    }
+
+    public void MoveTo(Location newLocation)
+    {
+      location = newLocation;
+    }
+  }
+}
